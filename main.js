3d8c88f86db2179b7ce1e010f05ec63ae4d345fc
khwww <khw4420@knu.ac.kr>
2024-07-10 01:30:35 +0900

    인자수정

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const getLatestNews = async () => {
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
-  console.log("test", data);
+  console.log("test", news);
 };
 
 getLatestNews();
