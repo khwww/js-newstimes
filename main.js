3b80c0939f5f805d6576068907697f84bd38362c
khwww <khw4420@knu.ac.kr>
2024-07-10 01:21:26 +0900

    api 배포주소 수정

--- main.js
+++ main.js
@@ -3,9 +3,7 @@
 let news = [];
 
 const getLatestNews = async () => {
-  const url = new URL(
-    "http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines"
-  );
+  const url = new URL("https://js-newtimes.netlify.app//top-headlines");
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
