4a37d2a35f4f625b1651d4e02e5c265726d0e17a
khwww <khw4420@knu.ac.kr>
2024-07-11 01:45:24 +0900

    news list

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 // const API_KEY =
 
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
-  const url = new URL("https://js-newtimes.netlify.app/top-headlines");
+  const url = new URL(
+    "https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines"
+  );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("test", news);
+  newsList = data.articles;
+  render();
+  console.log("test", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) =>
+        `<div class="row news">
+          <div class="col-lg-4"><img class="news-img-size" 
+          src="${news.urlToImage}"}" /></div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.desc}ription</p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+          </div>
+        </div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
